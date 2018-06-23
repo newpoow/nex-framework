80641b64e2857201a2f50f00c0eb10331ba3ebf2
Ney Pinheiro <ney.ps@hotmail.com>
2018-06-23 09:28:13 -0300

    defined exceptions to the psr-11 standard

---
+++ src/Injection/Exceptions/ContainerException.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the "Nex Framework" software,
+ * A simple and efficient web framework written with PHP.
+ *
+ * For complete copyright and license information,
+ * see the LICENSE file that was distributed with this source code.
+ *
+ * @license MIT
+ * @author Ney Pinheiro
+ * @copyright (c) 2019 Nex Framework { https://github.com/newpoow/nex-framework }
+ */
+namespace Nex\Injection\Exceptions;
+
+use Psr\Container\ContainerExceptionInterface;
+
+/**
+ * Exception caused when a dependency cannot be built.
+ * @package Nex\Injection
+ */
+class ContainerException extends \RuntimeException implements ContainerExceptionInterface
+{
+}
---
+++ src/Injection/Exceptions/EntryNotFoundException.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the "Nex Framework" software,
+ * A simple and efficient web framework written with PHP.
+ *
+ * For complete copyright and license information,
+ * see the LICENSE file that was distributed with this source code.
+ *
+ * @license MIT
+ * @author Ney Pinheiro
+ * @copyright (c) 2019 Nex Framework { https://github.com/newpoow/nex-framework }
+ */
+namespace Nex\Injection\Exceptions;
+
+use Psr\Container\NotFoundExceptionInterface;
+
+/**
+ * Exception caused when no entries were found in the container.
+ * @package Nex\Injection
+ */
+class EntryNotFoundException extends \RuntimeException implements NotFoundExceptionInterface
+{
+}
