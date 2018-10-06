d14e91fd70a9e76f5785729e1527d04767e702be
Ney Pinheiro <ney.ps@hotmail.com>
2018-10-06 08:57:21 -0300

    defined an http exception with code 405

---
+++ src/Http/Exceptions/MethodNotAllowedHttpException.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the "Nex Framework" software,
+ * A simple and efficient web framework written with PHP.
+ *
+ * For complete copyright and license information,
+ * see the LICENSE file that was distributed with this source code.
+ *
+ * @license MIT
+ * @author Ney Pinheiro
+ * @copyright (c) 2019 Nex Framework { https://github.com/newpoow/nex-framework }
+ */
+namespace Nex\Http\Exceptions;
+
+/**
+ * Exception caused when the http verb used is not supported.
+ * @package Nex\Http
+ */
+class MethodNotAllowedHttpException extends HttpException
+{
+    /** @var string[] */
+    protected $allowedMethods = array();
+
+    /**
+     * The http verb used is not supported.
+     * @param array $allowed
+     * @param \Throwable|null $previous
+     * @param array $headers
+     */
+    public function __construct(array $allowed, \Throwable $previous = null, array $headers = [])
+    {
+        $this->allowedMethods = $allowed;
+        $headers['Allow'] = strtoupper(implode(', ', $allowed));
+
+        parent::__construct(sprintf(
+            "The requested resource is not available for the HTTP method. Supported methods: '%s'.",
+            $headers['Allow']
+        ), 405, $previous, $headers);
+    }
+
+    /**
+     * Get the http methods allowed.
+     * @return string[]
+     */
+    public function getAllowedMethods(): array
+    {
+        return $this->allowedMethods;
+    }
+}
