08e9746d231f340cee7b6544c0da92aa3676b9ef
Ney Pinheiro <ney.ps@hotmail.com>
2018-11-30 19:07:13 -0200

    defined the exception used by the parsers

---
+++ src/Configuration/Exceptions/ParserException.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the "Nex Framework" software,
+ * A simple and efficient web framework written with PHP.
+ *
+ * For complete copyright and license information,
+ * see the LICENSE file that was distributed with this source code.
+ *
+ * @license MIT
+ * @author Ney Pinheiro
+ * @copyright (c) 2019 Nex Framework { https://github.com/newpoow/nex-framework }
+ */
+namespace Nex\Configuration\Exceptions;
+
+/**
+ * Exception caused when there are errors in the schema of the file being parsed.
+ * @package Nex\Configuration
+ */
+class ParserException extends \RuntimeException
+{
+}
